425dbc0e064d1d9569821083d4355cda3a3ae17f
Yury Landau <yurylandau@gmail.com>
2024-05-15 09:07:53 -0300

    fix: alterar arquivos vm para html

--- src/main/java/ti1/utils/Path.java
+++ src/main/java/ti1/utils/Path.java
@@ -30,15 +30,15 @@ public class Path {
     }
 
     public static class Template {
-        public final static String HOME = "/velocity/index/index.vm";
-        public final static String INDEX = "/velocity/index/index.vm";
-        public final static String FINANCIAL_REGISTRATION = "/velocity/financial-registration/index.vm";
-        public final static String PERSONAL_REGISTRATION = "/velocity/personal-registration/index.vm";
-        public final static String CALENDAR = "/velocity/calendar/index.vm";
-        public final static String INFLATION_CHART = "/velocity/inflation-chart/index.vm";
-        public final static String CALCULATOR = "/velocity/calculator/index.vm";
-        public final static String PLANNING_CHART = "/velocity/planning-chart/index.vm";
-        public final static String SPENDING_CHART = "/velocity/spending-chart/index.vm";
-        public static final String NOT_FOUND = "/velocity/notFound.vm";
+        public final static String HOME = "/velocity/index/index.html";
+        public final static String INDEX = "/velocity/index/index.html";
+        public final static String FINANCIAL_REGISTRATION = "/velocity/financial-registration/index.html";
+        public final static String PERSONAL_REGISTRATION = "/velocity/personal-registration/index.html";
+        public final static String CALENDAR = "/velocity/calendar/index.html";
+        public final static String INFLATION_CHART = "/velocity/inflation-chart/index.html";
+        public final static String CALCULATOR = "/velocity/calculator/index.html";
+        public final static String PLANNING_CHART = "/velocity/planning-chart/index.html";
+        public final static String SPENDING_CHART = "/velocity/spending-chart/index.html";
+        public static final String NOT_FOUND = "/velocity/notFound.html";
     }
 }
